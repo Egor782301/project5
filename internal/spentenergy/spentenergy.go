2d893a8c4459fe1973f058b059d85e15f8e322c6
Egor <egorvoroncov15@gmail.com>
2026-06-24 18:05:33 +0700

    5 итерация

--- internal/spentenergy/spentenergy.go
+++ internal/spentenergy/spentenergy.go
@@ -14,7 +14,6 @@ const (
 )
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	// TODO: реализовать функцию
 	if duration <= 0 || steps <= 0 || height <= 0 || weight <= 0 {
 		return 0, errors.New("WalkingSpentCalories - неверные данные")
 	}
@@ -48,7 +47,6 @@ func MeanSpeed(steps int, height float64, duration time.Duration) float64 {
 }
 
 func Distance(steps int, height float64) float64 {
-	// TODO: реализовать функцию
 	if steps <= 0 || height <= 0 {
 		return 0
 	}
